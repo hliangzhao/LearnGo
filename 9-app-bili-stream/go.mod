d36a53dee83b0ad424760f8055c8a3450e2c5c9a
hliangzhao <hliangzhao@zju.edu.cn>
2021-09-08 20:48:46 +0800

    Add part 9

---
+++ 9-app-bili-stream/go.mod
@@ -0,0 +1,5 @@
+module 9-app-bili-stream
+
+go 1.15
+
+require github.com/JimmyZhangJW/biliStreamClient v0.2.1
---
+++ 9-app-bili-stream/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	bili "github.com/JimmyZhangJW/biliStreamClient"
+	`log`
+)
+
+func main() {
+	biliClient := bili.New()
+	err := biliClient.Connect(22996546)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	defer biliClient.Disconnect()
+	for {
+		// 从通过中取数据并根据其类型出发不同的操作
+		packBody := <- biliClient.Ch
+		switch packBody.Cmd {
+		case "DANMU_MSG":
+			msg, _ := packBody.ParseDanmu()
+			log.Println(msg)
+			// 拿到msg之后，可以结合腾讯云提供的接口将弹幕用语音的方式朗读出来
+		case "SEND_GIFT":
+			log.Println(packBody.ParseGift())
+		case "COMBO_SENT":
+			log.Println(packBody.ParseGiftCombo())
+		default:
+			log.Println(packBody.Cmd)
+		}
+	}
+}
